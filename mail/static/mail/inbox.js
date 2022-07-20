6f21cfd683eca4d3f1e039867ec056c13f4e10bd
Brett Edmonds <notquitealibrarian@gmail.com>
2022-07-19 19:31:55 -0500

    Added send functionality to inbox.js.  Note, no validations on any fields.  Also no way to see sent email yet, but can verify that API worked correctly in the JS console.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Selects the form itself, NOT the submit button, and when the user clicks submit, runs the send_email function
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +33,30 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(event) {
+
+  // Prevents the "normal" behavior from this type of event
+  event.preventDefault()
+
+  // Uses the provided API to "send" emails
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+
+        // Note, could also use querySelector here
+        recipients: document.getElementById('compose-recipients').value,
+        subject: document.getElementById('compose-subject').value,
+        body: document.getElementById('compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  })
+
+  // Loads the sent mailbox after sending message.
+  .then(response => load_mailbox('sent'));
 }
